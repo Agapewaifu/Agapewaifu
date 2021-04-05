0dca155b3136809f37e4f42fd6cbe61e749943fd
Agape <2bmillerb@gmail.com>
2021-04-04 20:07:33 -0700

    Changing readme

--- README.py
+++ README.py
@@ -1,5 +1,14 @@
+import sys
+
+pyV = sys.version
+
+print("""
 - 👋 Hi, I’m @Agapewaifu
 - 👀 I’m interested in python and web backend development
 - 🌱 I’m currently learning python and various things through building projects
 - 💞️ I’m looking to collaborate on a website with my husband
 - 📫 @Agapewaifu on twitter
+""")
+
+print("You just ran my readme on")
+print(pyV)
